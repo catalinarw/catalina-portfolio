0d721affed807d5df6618f6d9f69ddf89aeb9709
Catalina Williams <catalina@MacBookAir.home.local>
2026-06-08 05:24:59 -0600

    forgot to save content for last commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <main>
+      <h1>About Me</h1>
+
+      <p>
+        I'm a web developer specializing in front-end development,
+        responsive design, WordPress, and digital user experiences.
+      </p>
+    </main>
+  );
+}
+
+export default About;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+function Contact() {
+  return (
+    <main>
+      <h1>Contact</h1>
+
+      <p>Email: catalinarose@icloud.com</p>
+
+      <p>LinkedIn</p>
+
+      <p>GitHub</p>
+    </main>
+  );
+}
+
+export default Contact;
