90534f357682277ec3db159d0578334a08c99474
Catalina Williams <catalina@MacBookAir.home.local>
2026-06-08 05:14:48 -0600

    built home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+function Home() {
+  return (
+    <main>
+      <h1>Catalina Williams</h1>
+
+      <h2>
+        Front-End Developer | WordPress Specialist | Web Designer
+      </h2>
+
+      <p>
+        Web developer with 4+ years of experience designing,
+        rebuilding, and maintaining websites across WordPress,
+        HubSpot CMS, and custom web platforms.
+      </p>
+    </main>
+  );
+}
+
+export default Home;
